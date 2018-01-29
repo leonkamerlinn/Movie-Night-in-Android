aaff2c86cdfb8e26be8f45be620d485b133884f7
Leon <leonkamerlinn@gmail.com>
2018-01-29 23:31:53 +0100

    keystore.properties

--- app/src/main/java/com/example/leon/movienightinandroid/MainActivity.java
+++ app/src/main/java/com/example/leon/movienightinandroid/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.leon.movienightinandroid;
 
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, BuildConfig.MESSAGE, Toast.LENGTH_LONG).show();
     }
 }
