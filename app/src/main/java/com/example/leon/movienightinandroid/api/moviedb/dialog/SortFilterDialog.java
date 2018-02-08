312d47e6d4ac92c28d9033c737545fdc3062b789
Leon Kamerlin <leon.kamerlin@nybble.hr>
2018-02-08 15:58:23 +0100

    added ratingbar to dialog

--- app/src/main/java/com/example/leon/movienightinandroid/api/moviedb/dialog/SortFilterDialog.java
+++ app/src/main/java/com/example/leon/movienightinandroid/api/moviedb/dialog/SortFilterDialog.java
@@ -2,13 +2,13 @@ package com.example.leon.movienightinandroid.api.moviedb.dialog;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.view.ViewGroup;
 
 import com.example.leon.movienightinandroid.R;
 
@@ -65,5 +65,11 @@ public class SortFilterDialog extends DialogFragment {
                 })
                 .setNegativeButton(R.string.lb_onboarding_accessibility_next, (dialog, id) -> getDialog().cancel());
         return builder.create();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
