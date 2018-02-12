9becb0a2d207a090e55c49e9ec777aac38bf3275
Leon Kamerlin <leon.kamerlin@nybble.hr>
2018-02-12 14:16:40 +0100

    update

--- app/src/main/java/com/example/leon/movienightinandroid/api/moviedb/dialog/SortFilterDialog.java
+++ app/src/main/java/com/example/leon/movienightinandroid/api/moviedb/dialog/SortFilterDialog.java
@@ -2,14 +2,20 @@ package com.example.leon.movienightinandroid.api.moviedb.dialog;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.widget.Toast;
 
+import com.example.leon.movienightinandroid.R;
+import com.example.leon.movienightinandroid.databinding.DialogSortFilterBinding;
 import com.example.leon.movienightinandroid.di.component.DaggerDialogFragmentComponent;
 import com.example.leon.movienightinandroid.di.component.DialogFragmentComponent;
 import com.example.leon.movienightinandroid.di.module.SortFilterDialogModule;
+import com.jakewharton.rxbinding2.widget.RxAdapterView;
 
 import javax.inject.Inject;
 
@@ -46,7 +52,7 @@ public class SortFilterDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        /*DialogSortFilterBinding binding = DataBindingUtil.inflate(LayoutInflater.from(getContext()), R.layout. dialog_sort_filter, null, false);
+        DialogSortFilterBinding binding = DataBindingUtil.inflate(LayoutInflater.from(getContext()), R.layout. dialog_sort_filter, null, false);
 
         String[] sortItems = getResources().getStringArray(R.array.sort_items);
         RxAdapterView.itemSelections(binding.sortSpinner)
@@ -62,10 +68,10 @@ public class SortFilterDialog extends DialogFragment {
 
                 })
                 .setNegativeButton(R.string.cancel, (dialog, id) -> getDialog().cancel())
-                .create();*/
+                .create();
 
 
-        AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
+    /*    AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
         alertDialog.setTitle(message);
         alertDialog.setMessage("Alert message to be shown");
         alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
@@ -76,7 +82,7 @@ public class SortFilterDialog extends DialogFragment {
                 });
         alertDialog.show();
 
-        return alertDialog;
+        return alertDialog;*/
 
     }
 
